65eafd5f63196253533f2349971ee5808ad26e69
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-01-07 16:49:03 -0800

    Products View

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from products.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home , name='home')
+    # path('', PageListView.as_view(), name='wiki-list-page'),
 ]
